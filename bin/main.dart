bd9f33f7a9e27cb1d17dd7ff71d255024ee3e255
Jacob MacDonald <jakemac53@gmail.com>
2018-05-07 17:36:24 -0700

    depend on latest dartsicord, add lotus-ws queue

--- bin/main.dart
+++ bin/main.dart
@@ -5,12 +5,13 @@ import 'package:dartsicord/dartsicord.dart';
 import 'package:hades_simulator/discord.dart';
 
 final queues = <String, StarQueue>{
-  '!lotus-rs5': new RedStarQueue('lotus-rs5'),
-  '!lotus-rs6': new RedStarQueue('lotus-rs6'),
   '!wey-rs5': new RedStarQueue('wey-rs5'),
   '!wey-rs6': new RedStarQueue('wey-rs6'),
   '!wey-rs7': new RedStarQueue('wey-rs7'),
   '!wey-ws': new WhiteStarQueue('wey-ws'),
+  '!lotus-rs5': new RedStarQueue('lotus-rs5'),
+  '!lotus-rs6': new RedStarQueue('lotus-rs6'),
+  '!lotus-ws': new WhiteStarQueue('lotus-ws'),
 };
 
 main() async {
@@ -25,33 +26,38 @@ main() async {
 
     if (command == '!help') {
       message = await help(event);
-    } else if (command == '!cleanup') {
-      await cleanup(event, args);
+      new Future.delayed(new Duration(seconds: 60), () async {
+        await message.delete();
+      });
+      try {
+        await event.message.delete();
+      } catch (_) {}
+    } else if (command == '!tidy') {
+      await tidy(event, args);
     } else {
       var queue = queues[command];
       if (queue != null) {
         message = await queue.handleCommand(args, event);
+        new Future.delayed(new Duration(seconds: 15), () async {
+          await message.delete();
+        });
       }
     }
 
-    if (message != null) {
-      new Future.delayed(new Duration(seconds: 10), () {
-        return message.delete();
-      });
-    }
+    if (message != null) {}
   });
 
   await client.connect(new File('bin/client_token.txt').readAsStringSync());
 }
 
-Future<void> cleanup(MessageCreateEvent event, List<String> args) async {
+Future<void> tidy(MessageCreateEvent event, List<String> args) async {
   var deleteCount = args.isEmpty ? 10 : int.tryParse(args.first) ?? 10;
   var channel = event.message.channel;
   var messages = await channel.getMessages(
       limit: deleteCount,
       base: event.message,
       downloadType: MessageDownloadType.before);
-  await channel.bulkDeleteMessages(messages);
+  await channel.bulkDeleteMessages(messages.toList()..add(event.message));
 }
 
 Future<Message> help(MessageCreateEvent event) {
