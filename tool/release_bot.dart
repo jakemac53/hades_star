637716fa8e2f8297f829c7e43e136b81860cf3f2
Jacob MacDonald <jakemac53@gmail.com>
2018-11-25 11:00:21 -0800

    allow releasing to a specified dir

--- tool/release_bot.dart
+++ tool/release_bot.dart
@@ -1,29 +1,35 @@
 import 'dart:io';
 import 'package:path/path.dart' as p;
 
-main() async {
-  print('cleaning up old release dirs');
-  var existing = Directory('release');
-  if (await existing.exists()) {
-    await existing.delete(recursive: true);
+main(List<String> args) async {
+  var outputPath = 'release';
+  if (args.length == 1) {
+    outputPath = args.first;
+  } else if (args.length > 1) {
+    throw 'Only expected on arg, the relase dir!';
   }
-  await Directory(p.join('release', 'bin')).create(recursive: true);
+  print('cleaning up old release dirs');
+  var outputDir = Directory(outputPath);
+  if (await outputDir.exists()) {
+    await outputDir.delete(recursive: true);
+  }
+  await Directory(p.join(outputDir.path, 'bin')).create(recursive: true);
   print('snapshotting bootstrap script');
   await Process.run('dart', [
-    '--snapshot=${p.join('release', 'bin', 'macvault_bootstrap.dart.snapshot')}',
+    '--snapshot=${p.join(outputDir.path, 'bin', 'macvault_bootstrap.dart.snapshot')}',
     p.join('bin', 'macvault_bootstrap.dart'),
   ]);
   print('snapshotting main vault script');
   await Process.run('dart', [
-    '--snapshot=${p.join('release', 'bin', 'macvault.dart.snapshot')}',
+    '--snapshot=${p.join(outputDir.path, 'bin', 'macvault.dart.snapshot')}',
     p.join('bin', 'macvault.dart'),
   ]);
   print('copying dart vm and secrets');
   await File(Platform.executable)
-      .copy(p.join('release', p.basename(Platform.executable)));
+      .copy(p.join(outputDir.path, p.basename(Platform.executable)));
   await File(p.join('bin', 'macvault_client_token.txt'))
-      .copy(p.join('release', 'bin', 'macvault_client_token.txt'));
+      .copy(p.join(outputDir.path, 'bin', 'macvault_client_token.txt'));
   await File(p.join('bin', 'macvault_db.txt'))
-      .copy(p.join('release', 'bin', 'macvault_db.txt'));
+      .copy(p.join(outputDir.path, 'bin', 'macvault_db.txt'));
   print('done!');
 }
